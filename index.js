1a571d46de0e7654a1cc1ffe483a4a6bb5eb5606
Kyushik <kyushik95@gmail.com>
2023-01-15 16:55:36 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown(number) {
+    while (number > 0) {
+      console.log(number);
+      number--;
+    }
+    console.log(0);
+  }
+
+function writeCards(people, eventName) {
+    let totalMessages = [];
+    for (let i = 0; i < people.length; i++) {
+        let thankYouMessage = `Thank you, ${people[i]}, for the wonderful ${eventName} gift!`;
+        totalMessages.push(thankYouMessage);
+    };
+    return totalMessages;
+};
